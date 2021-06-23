c9eba008d7454103522ac2617558f753a4ee6791
Guddumbhagyasri <g.bhagya9@gmail.com>
2021-06-22 23:20:55 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ git fetch
 
 # Loop through all remote merged branches
 for branch in $(git branch -r --merged | grep -v HEAD | grep -v QA | grep -v QA | grep -v master | sed /\*/d); do
-        if [ -z "$(git log -1 --since='Jun 15, 2020' -s ${branch})" ]; then
+        if [ -z "$(git log -1 --since='Jun 22, 2021' -s ${branch})" ]; then
                 echo -e `git show --format="%ci %cr %an" ${branch} | head -n 1` \\t$branch
                 remote_branch=$(echo ${branch} | sed 's#origin/##' )
                 #git push origin --delete ${remote_branch}tt
